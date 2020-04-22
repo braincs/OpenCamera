a4fc7138440bcd877971a6839562dec2e2e06beb
shuai <8019969+braincs@users.noreply.github.com>
2020-04-22 17:48:19 +0800

    demo: static object be overide by instance set

---
+++ app/src/androidTest/java/com/braincs/attrsc/opencamera/ImportantObject.java
@@ -0,0 +1,17 @@
+package com.braincs.attrsc.opencamera;
+
+/**
+ * Created by Shuai
+ * 22/04/2020.
+ */
+public class ImportantObject {
+    private String name;
+
+    public ImportantObject(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
